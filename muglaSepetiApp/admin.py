8a67868555015eee01b42287a9ed9e49f4c292af
furkankykc <furkanfbr@gmail.com>
2020-08-14 05:23:51 +0300

    minor refactoring on models.py added admin.py permissions,filters,default model actions and so more

--- muglaSepetiApp/admin.py
+++ muglaSepetiApp/admin.py
@@ -1,19 +1,171 @@
 from django.contrib import admin
+
 from muglaSepetiApp.models import *
 
 # Register your models here.
 admin.site.register(BucketEntry)
-admin.site.register(Bucket)
+# admin.site.register(Bucket)
 admin.site.register(Address)
-admin.site.register(Entry)
+# admin.site.register(Entry)
 # admin.site.register(Company)
 admin.site.register(Profile)
-admin.site.register(FoodGroup)
-admin.site.register(FoodCategory)
-admin.site.register(Menu)
+
+
+# admin.site.register(FoodGroup)
+# admin.site.register(FoodCategory)
+
+
+# admin.site.register(Menu)
+
+# default admin model for company releated views
+class DefaultAdminModel(admin.ModelAdmin):
+    class Meta:
+        abstract = True
+
+    def save_model(self, request, obj, form, change):
+        if not request.user.is_superuser:
+            # if user has not multiple companies
+            if Company.objects.filter(owner=request.user).count() <= 1:
+                obj.owner = request.user
+        super(DefaultAdminModel, self).save_model(request, obj, form, change)
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(DefaultAdminModel, self).get_form(request, obj, **kwargs)
+        if 'slug' in form.base_fields:
+            form.base_fields['slug'].disabled = True
+            form.base_fields['slug'].help_text = "This field is not editable"
+        # if 'owner' in form.base_fields:
+        #     form.base_fields['owner'].disabled = True
+        #     form.base_fields['owner'].help_text = "This field is not editable"
+
+        return form
+
+    def get_readonly_fields(self, request, obj=None):
+        local_read_only_fields = ()
+        if obj and not request.user.is_superuser:
+            if hasattr(obj, 'owner'):
+                local_read_only_fields += ('owner',)
+            # if user has not multiple companies
+            if Company.objects.filter(owner=request.user).count() <= 1:
+                if hasattr(obj, 'company'):
+                    local_read_only_fields += ('company',)
+        return self.readonly_fields + local_read_only_fields
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if not request.user.is_superuser:
+            # for extra security
+            if db_field.name == "company":
+                kwargs["queryset"] = Company.objects.filter(owner=request.user)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def get_queryset(self, request):
+        qs = super(DefaultAdminModel, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(company__owner=request.user)
 
 
 @admin.register(Company)
-class CompanyAdmin(admin.ModelAdmin):
-    list_display = ('name', 'get_is_open')
-    # fields = ('__all__',)
+class CompanyAdmin(DefaultAdminModel):
+    list_display = ('name', 'active_menu', 'open_at', 'close_at', 'is_currently_open')
+    prepopulated_fields = {'slug': ['name']}
+
+    fields = (
+        'owner', 'slug', 'name', 'logo', 'address', 'active_menu', 'is_open', 'open_at', 'close_at', 'phone', 'email',
+        'instagram',
+        'facebook', 'twitter')
+
+    @staticmethod
+    def is_currently_open(obj):
+        return obj.get_is_open()
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(DefaultAdminModel, self).get_form(request, obj, **kwargs)
+        if 'active_menu' in form.base_fields:
+            if obj is not None:
+                print(obj.id)
+                form.base_fields['active_menu'].queryset = Menu.objects.filter(company__id=obj.id)
+        return form
+
+    def get_queryset(self, request):
+        qs = super(DefaultAdminModel, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(owner=request.user)
+
+
+@admin.register(Menu)
+class MenuAdmin(DefaultAdminModel):
+    list_display = ('name', 'count_of_entries')
+    # fields = ('name')
+    filter_horizontal = ('entry_list',)
+
+    def count_of_entries(self, obj):
+        return obj.entry_list.count()
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if not request.user.is_superuser:
+            if db_field.name == "entry_list":
+                kwargs["queryset"] = Entry.objects.filter(company__owner=request.user)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+@admin.register(FoodGroup)
+class FoodGroupAdmin(DefaultAdminModel):
+    list_display = ('name', 'company')
+
+
+@admin.register(FoodCategory)
+class FoodCategoryAdmin(DefaultAdminModel):
+    list_display = ('name', 'group', 'company')
+
+
+@admin.register(Entry)
+class EntryAdmin(DefaultAdminModel):
+    list_filter = ('company', 'category')
+    list_display = ('name', 'detail', 'price', 'company', 'category')
+
+
+def make_check(modeladmin, request, queryset):
+    for i in queryset.all():
+        i.check_order()
+
+
+def make_on_the_way(modeladmin, request, queryset):
+    for i in queryset.all():
+        i.order_on_the_way()
+
+
+def make_delivered(modeladmin, request, queryset):
+    for i in queryset.all():
+        i.deliver_order()
+
+
+def make_cancel(modeladmin, request, queryset):
+    for i in queryset.all():
+        i.cancel_order()
+
+
+make_check.short_description = "Mark selected orders as checked"
+make_on_the_way.short_description = "Mark selected orders as on the way"
+make_delivered.short_description = "Mark selected orders as delivered"
+make_cancel.short_description = "Mark selected orders as canceled"
+
+
+@admin.register(Bucket)
+class BucketAdmin(admin.ModelAdmin):
+    exclude = ['is_checked', 'is_ordered', 'is_delivered', 'is_on_the_way', 'is_deleted', 'checked_at', 'delivered_at',
+               'deleted_at', 'on_the_way_at']
+    list_display = ('get_products', 'delivery_note', 'order_address', 'order_phone', 'get_borrow', 'company')
+    list_filter = ('company',)
+    ordering = ['ordered_at']
+    actions = [make_check, make_on_the_way, make_delivered, make_cancel]
+
+    def get_products(self, obj):
+        return ",".join([i.name for i in [p.entry for p in obj.order_list.all()]])
+
+    def get_queryset(self, request):
+        qs = super(BucketAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(company__owner=request.user, is_ordered=True)
--- muglaSepetiApp/models.py
+++ muglaSepetiApp/models.py
@@ -7,6 +7,7 @@ from django.db.models import F, Sum
 from django.dispatch import receiver
 from django.utils import timezone
 from django.db.models.signals import post_save
+from django.utils.text import slugify
 
 phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
                              message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
@@ -54,12 +55,14 @@ class Company(models.Model):
     name = models.CharField(max_length=20)
     logo = models.ImageField(upload_to=get_image_path, blank=True, null=True)
     slug = models.SlugField(blank=True)
-    active_menu = models.ForeignKey('Menu', on_delete=models.CASCADE, blank=True, null=True)
+    active_menu = models.ForeignKey('Menu', on_delete=models.CASCADE, blank=True, null=True,
+                                    related_name='comp_active_menu')
     phone = models.CharField(validators=[phone_regex], max_length=17, blank=True,
                              null=True)
     email = models.CharField(validators=[email_regex], max_length=50, blank=True)
+    address = models.CharField(max_length=500)
     open_at = models.TimeField(default=timezone.now)
-    closed_at = models.TimeField(default=timezone.now)
+    close_at = models.TimeField(default=timezone.now)
     is_open = models.BooleanField(default=False)
     instagram = models.URLField(blank=True)
     facebook = models.URLField(blank=True)
@@ -68,13 +71,18 @@ class Company(models.Model):
     youtube = models.URLField(blank=True)
     pinterest = models.URLField(blank=True)
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Company, self).save(*args, **kwargs)
+
     def get_is_open(self):
+
         curr_hour = timezone.now().hour
         curr_minute = timezone.now().minute
         open_hour = self.open_at.hour
         open_minute = self.open_at.minute
-        close_hour = self.closed_at.hour
-        close_minute = self.closed_at.minute
+        close_hour = self.close_at.hour
+        close_minute = self.close_at.minute
         if self.is_open:
             if (curr_hour >= open_hour and curr_minute >= open_minute) and (
                     curr_hour <= close_hour and curr_minute <= close_minute):
@@ -91,7 +99,7 @@ class FoodGroup(models.Model):
         verbose_name_plural = 'Menü Grupları'
 
     name = models.CharField(max_length=20, verbose_name='isim')
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True, verbose_name='sahip')
+    company = models.ForeignKey(Company, on_delete=models.SET_NULL, blank=True, null=True, verbose_name='sahip')
 
     def __str__(self):
         return self.name
@@ -104,7 +112,7 @@ class FoodCategory(models.Model):
 
     name = models.CharField(max_length=20, verbose_name='isim')
     image = models.ImageField(upload_to=get_image_path, blank=True, null=True, verbose_name='kapak fotoğrafı')
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True, verbose_name='sahip')
+    company = models.ForeignKey(Company, on_delete=models.SET_NULL, blank=True, null=True, verbose_name='sahip')
     group = models.ForeignKey(FoodGroup, on_delete=models.SET_NULL, blank=True, null=True, verbose_name='grup')
 
     def __str__(self):
@@ -138,7 +146,7 @@ class Entry(models.Model):
 class Menu(models.Model):
     name = models.CharField(max_length=50)
     entry_list = models.ManyToManyField(Entry)
-    owner = models.ForeignKey(Company, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -156,16 +164,21 @@ class BucketEntry(models.Model):
 class Bucket(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    delivery_note = models.CharField(max_length=500, blank=True)
     order_list = models.ManyToManyField(BucketEntry, blank=True)
     order_address = models.CharField(max_length=500)
     order_phone = models.CharField(max_length=500)
-    delivered_at = models.DateTimeField(blank=True, null=True)
     checked_at = models.DateTimeField(blank=True, null=True)
     ordered_at = models.DateTimeField(blank=True, null=True)
+    on_the_way_at = models.DateTimeField(blank=True, null=True)
+    delivered_at = models.DateTimeField(blank=True, null=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
     is_ordered = models.BooleanField(default=False, )
     is_checked = models.BooleanField(default=False)
+    is_on_the_way = models.BooleanField(default=False, )
     is_delivered = models.BooleanField(default=False, blank=True)
     is_deleted = models.BooleanField(default=False, blank=True)
+
     PAYMENT_OPTIONS = (
         ('N', 'Nakit'),
         ('K', 'Kredi Kartı'),
@@ -186,13 +199,29 @@ class Bucket(models.Model):
         self.save()
 
     def check_order(self):
-        self.is_checked = True
-        self.checked_at = timezone.now()
-        self.save()
+        if self.is_ordered:
+            self.is_checked = True
+            self.checked_at = timezone.now()
+            self.save()
+
+    def cancel_order(self):
+        if self.is_ordered:
+            self.is_checked = False
+            self.is_deleted = True
+            self.deleted_at = timezone.now()
+            self.save()
+
+    def order_on_the_way(self):
+        if self.is_checked:
+            self.is_on_the_way = True
+            self.on_the_way_at = timezone.now()
+            self.save()
 
     def deliver_order(self):
-        self.is_delivered = True
-        self.delivered_at = timezone.now()
+        if self.is_on_the_way:
+            self.is_delivered = True
+            self.delivered_at = timezone.now()
+            self.save()
 
     def add_entry(self, entry: Entry, count: int = 1):
         obj, _ = self.order_list.get_or_create(entry_id=entry.id)
@@ -205,7 +234,7 @@ class Bucket(models.Model):
     def get_borrow(self):
         sum = 0
         if self.order_list.exists():
-            order_list = self.order_list.filter(is_deleted=False, is_checked=True)
+            order_list = self.order_list.all()
             if order_list.count() > 0:
                 sum = list(order_list.annotate(total_spent=Sum(
                     F('price') *
