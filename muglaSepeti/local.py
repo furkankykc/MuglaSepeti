d8a097b1734c89c36b8349db235b5991000cf5b7
Furkan kıyıkçı <furkanfbr@gmail.com>
2020-09-23 11:58:13 +0300

    Delete local.py

--- muglaSepeti/local.py
+++
@@ -1,9 +0,0 @@
-import os
-
-DEBUG = True
-ALLOWED_HOSTS = ['*']
-HTTP_METHOD = 'http'
-SITE_URL = 'localhost:8000'
-STATICFILES_DIRS = (os.path.join('static'),)
-STATIC_ROOT = ""
-# STATIC_ROOT = os.path.join('static')
